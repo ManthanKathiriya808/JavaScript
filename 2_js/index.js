e191fc4a94a5bdf91daf46d9922bfba4a4f3f29b
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-12-02 17:38:15 +0530

    sdfg

---
+++ 2_js/index.js
@@ -0,0 +1,29 @@
+// let num = 30;
+// let string= "fdg";
+// console.log(num)
+
+// console.log(string)
+// console.log(typeof num)
+// console.log(typeof string)
+
+
+let variable = 40;
+let var2 = String(variable)
+
+console.log(typeof var2)
+
+
+
+let arr= ["manthan" , 33 , true];
+
+console.log(arr[0])
+console.log(arr)
+
+
+let obj = {
+    name: "apache",
+    model: 2022,
+    fule: true
+}
+
+console.log( obj)
