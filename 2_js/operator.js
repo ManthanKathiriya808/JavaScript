ee17b2c88b34c967c4a2f9b062f12243c88b46f8
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-12-03 15:38:21 +0530

    sdfg

---
+++ 2_js/operator.js
@@ -0,0 +1,20 @@
+let a=5;
+let b= 5;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// b+=5
+
+// console.log(b)
+
+// let text1 = "John";
+// let text2 = "Doe";
+
+// console.log(text1  > text2)
+
+let x=4;
+console.log(x!=5)
