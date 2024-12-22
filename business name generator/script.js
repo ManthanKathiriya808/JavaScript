7b01ab1f0e060c18f06968a4e87309263b77f306
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2024-12-22 13:41:17 +0530

    dsfg

---
+++ business name generator/script.js
@@ -0,0 +1,40 @@
+// create a business name generator by combining list of adjactives and shop name and another word
+
+// adjactive :
+// crazy 
+// amazing 
+// fire
+
+// shop name :
+
+// engine
+// foods
+// garments
+
+// another word:
+
+// Bros
+// limited
+// hub
+
+
+
+
+// function adjactive(){
+//     let arr = ["crazy","amazing","fire"];
+//     let arr1 = [5,3,0,5,4,6,2];
+
+// for(let i =0; i<arr1.length;i++){
+
+//     console.log(arr1[i])
+// }
+
+//     // console.log(arr)
+//     // console.log(arr1.sort())
+
+// }
+
+
+// adjactive()
+
+// //    console.log(Array.from("manthan"))
